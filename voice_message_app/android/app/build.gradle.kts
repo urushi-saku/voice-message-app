74f27c6534b6da48db3532ee55306cb5e5eab09e
xiaox <xiaox@example.com>
2026-02-19 16:54:44 +0900

    fix: core library desugaring を有効化 (flutter_local_notifications 要件)

--- voice_message_app/android/app/build.gradle.kts
+++ voice_message_app/android/app/build.gradle.kts
@@ -13,6 +13,7 @@ android {
     ndkPath = "/home/xiaox/Android/ndk/27.0.12077973"
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -43,4 +44,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
